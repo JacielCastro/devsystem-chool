ec3071673f86fa58da9fe38aa5d055541a46de1c
Jaciel Castro <jacielcastro100@gmail.com>
2026-05-14 21:23:24 -0300

    :books: -- Atualização --

---
+++ src/config/database.js
@@ -0,0 +1,21 @@
+import mysql from 'mysql2'
+import dotenv from 'dotenv'
+
+const bdCenexao = mysql.createConnection({
+    host: 'local',
+    port: 3306,
+    user: 'root',
+    password: 'BemVindo!',
+    database: 'bd_academico',
+    charset: 'utf8mb4',
+    multipleStatements: true
+})
+bdCenexao.connect((error) => {
+    if (error) {
+        console.error('Erro ao conectar ao banco de ')
+        return
+    }
+    console.log('Conexão completa');
+})
+
+export default bdCenexao  
